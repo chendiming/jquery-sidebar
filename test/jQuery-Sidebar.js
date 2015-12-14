9a70a65d3eda8a6da66429173c8d34e7af2e7f1b
gavin <tianyu_0723@hotmail.com>
2015-12-14 17:46:27 +0800

    发布 1.0.0 版本

--- test/jQuery-Sidebar.js
+++ test/jQuery-Sidebar.js
@@ -1,7 +1,7 @@
 /**
  *  jQuery-Sidebar.js
- *  Version: 0.1.0
- *  https://github.com/gavin66/jQuery-Sidebar
+ *  Version: 1.0.0
+ *  http://github.com/gavin66/jQuery-Sidebar
  */
 
 ;
@@ -9,33 +9,42 @@
     "use strict";
 
     $.jqSidebar = function (options) {
+
+        /**
+         * ==================
+         *       插件参数
+         * ==================
+         */
+
         var setting = $.extend({
-            leftActive: false, // 左侧是否为默认激活状态
-            rightActive: false, // 右侧是否为默认激活状态
-
-            //leftMode: 'container-offset', // 左侧边栏的切换方式 默认为'offset'
-            leftMode: 'sidebar-offset',
-            rightMode: 'container-offset', // 右侧边栏的切换方式 sidebar-offset
-
-            autoClose: true, // 点击container 是否自动关闭两侧导航栏
-
-            isTurn: false, // 是否是交替显示模式
+            // sidebar-turn container-offset sidebar-offset
+            leftMode: 'container-offset', // 左侧边栏的切换方式
+            rightMode: 'container-offset', // 右侧边栏的切换方式
             leftTurnShow: 'jqsb-left-bg', // 左侧导航栏为左右交替变换时默认显示的元素
-            rightTurnShow: 'jqsb-right-bg' // 右侧导航栏为左右交替变换时默认显示的元素
-
+            rightTurnShow: 'jqsb-right-bg', // 右侧导航栏为左右交替变换时默认显示的元素
+            sideActive:'', // 哪边为激活状态 可为'left','right' 默认为空
+            autoClose: true // 点击container 是否自动关闭两侧导航栏
         }, options);
 
-        // 初始化的窗口大小
-        var windowWidth = $(window).width();
+
+        /**
+         * =====================
+         *       定义插件变量
+         * =====================
+         */
 
         // 左右两侧侧边栏是否是激活状态 初始化为false 不是激活状态
-        var leftActive = false;
-        var rightActive = false;
+        var leftActive = setting.sideActive === 'left' ? 1 : 0;
+        var rightActive = setting.sideActive === 'right' ? 1 : 0;
         var leftBgActive = false;
-        var leftSmActive = false;
         var rightBgActive = false;
+        var leftSmActive = false;
         var rightSmActive = false;
 
+        // 获取中间内容的容器
+        if ($('.jqsb-container').length) {
+            var $container = $('.jqsb-container');
+        }
         // 获取左侧边栏
         if ($('.jqsb-left').length) {
             var $left = $('.jqsb-left');
@@ -45,89 +54,200 @@
         if ($('.jqsb-left-sm').length) {
             var $left_sm = $('.jqsb-left-sm');
             var left_sm_width = $left_sm.width();
+            var left_sm_width_unit = $left_sm.css('width');
         }
         // 如果左侧边栏是大小切换方式的 获取大的块
         if ($('.jqsb-left-bg').length) {
             var $left_bg = $('.jqsb-left-bg');
             var left_bg_width = $left_bg.width();
+            var left_bg_width_unit = $left_bg.css('width');
         }
         // 获取右侧边栏
         if ($('.jqsb-right').length) {
             var $right = $('.jqsb-right');
+            var right_width_unit = $right.css('width');
         }
         // 如果右侧边栏是大小切换方式的 获取小的块
         if ($('.jqsb-right-sm').length) {
             var $right_sm = $('.jqsb-right-sm');
             var right_sm_width = $right_sm.width();
+            var right_sm_width_unit = $right_sm.css('width');
         }
         // 如果右侧边栏是大小切换方式的 获取大的块
         if ($('.jqsb-right-bg').length) {
             var $right_bg = $('.jqsb-right-bg');
             var right_bg_width = $right_bg.width();
-        }
-        // 获取中间内容的容器
-        if ($('.jqsb-container').length) {
-            var $container = $('.jqsb-container');
+            var right_bg_width_unit = $right_bg.css('width');
         }
 
 
-        // 初始化插件
-        function init() {
-            if ($container && $left && $left_sm && $left_bg && setting.isTurn) {
-                $container.css('margin-left', left_width_unit);// 初始 $container 内容容器的左外补 margin-left
-                $left.width(left_width_unit);
-                leftActive = true; // 如果左侧导航栏是大小栏交替隐藏模式的话,那么它就是激活状态
-                if (setting.leftTurnShow === 'jqsb-left-bg') { // 默认显示大的话,隐藏小的  反之,亦然
-                    $left_sm.hide();
-                    leftBgActive = true;
+        /**
+         * ==================
+         *       操作
+         * ==================
+         */
+
+        // 开关
+        function toggle(side) {
+            if (side === 'left' && $left) {
+                if (leftActive && $left_bg && $left_sm) {
+                    if (leftBgActive) {
+                        close('left');
+                    } else if (leftSmActive) {
+                        open('left');
+                    }
                 } else {
-                    $left_bg.hide();
-                    leftSmActive = true;
+                    if (!leftActive) {
+                        open('left');
+
+                    } else {
+                        close('left');
+                    }
                 }
-            } else if ($container && $left) {
-                if(setting.leftMode === 'sidebar-offset'){
-                    $left.css('transform', 'translate( -' + left_width_unit + ' )').addClass('sidebar-mode');
+            }
+            if (side === 'right' && $right) {
+                if (rightActive && $right_bg && $right_sm) {
+                    if (rightBgActive) {
+                        close('right');
+                    } else if (rightSmActive) {
+                        open('right');
+                    }
+                } else {
+                    if (!rightActive) {
+                        open('right');
+                    } else {
+                        close('right');
+                    }
                 }
             }
 
-            css();
+        }
+
+        // 打开
+        function open(side) {
+            var time = 1;
+            if($left && side === 'left'){
+                if(setting.rightMode !== 'sidebar-turn'){
+                    if(rightActive) time = 400;
+                    close('right'); // 开左侧,先关闭右侧
+                }
+                if(leftSmActive){
+                    animate('turn', [$left_sm, $left_bg], 'left', left_bg_width, function () {
+                        leftBgActive = true;
+                        leftSmActive = false;
+                    });
+                }else if(setting.leftMode === 'container-offset'){
+                    setTimeout(function(){
+                        animate('offset', [$container], '', left_width_unit, function () {
+                            leftActive = true;
+                        });
+                    },time);
+                }else if(setting.leftMode === 'sidebar-offset'){
+                    setTimeout(function(){
+                        animate('offset', [$left], '', left_width_unit, function () {
+                            leftActive = true;
+                        });
+                    },time);
+                }
+            }
+            if($right && side === 'right'){
+                if(setting.leftMode !== 'sidebar-turn'){
+                    if(leftActive) time = 400;
+                    close('left'); // 开左侧,先关闭右侧
+                }
+                if(rightSmActive){
+                    animate('turn', [$right_sm, $right_bg], 'right', right_bg_width, function () {
+                        rightBgActive = true;
+                        rightSmActive = false;
+                    });
+                }else if(setting.rightMode === 'container-offset'){
+                    setTimeout(function(){
+                        animate('offset', [$container], '', '-'+right_width_unit, function () {
+                            rightActive = true;
+                        });
+                    },time);
+                }else if(setting.rightMode === 'sidebar-offset'){
+                    setTimeout(function(){
+                        animate('offset', [$right], '', '-'+right_width_unit, function () {
+                            rightActive = true;
+                        });
+                    },time);
+                }
+            }
 
         }
 
-        init();
-
-        // 样式变化
-        function css() {
-
+        // 关闭
+        function close(side) {
+            if($left && side === 'left'){
+                if(leftBgActive){
+                    animate('turn', [$left_bg, $left_sm], 'left', left_sm_width, function () {
+                        leftBgActive = false;
+                        leftSmActive = true;
+                    });
+                }else if(setting.leftMode === 'container-offset'){
+                    animate('offset', [$container], '', '0px', function () {
+                        leftActive = false;
+                    });
+                }else if(setting.leftMode === 'sidebar-offset'){
+                    animate('offset', [$left], '', '0px', function () {
+                        leftActive = false;
+                    });
+                }
+            }
+            if($right && side === 'right'){
+                if(rightBgActive){
+                    animate('turn', [$right_bg, $right_sm], 'right', right_sm_width, function () {
+                        rightBgActive = false;
+                        rightSmActive = true;
+                    });
+                }else if(setting.rightMode === 'container-offset'){
+                    animate('offset', [$container], '', '0px', function () {
+                        rightActive = false;
+                    });
+                }else if(setting.rightMode === 'sidebar-offset'){
+                    animate('offset', [$right], '', '0px', function () {
+                        rightActive = false;
+                    });
+                }
+            }
         }
-
-        // 窗口大小改变时
-        $(window).on('resize', function () {
-            var resizedWindowWidth = $(window).width();
-
-        });
 
         // 动画
-        function animate(mode, selectors, amount, callback) {
+        function animate(mode, selectors,position,amount, callback) {
             // 动画类型是大小切换的
             if (mode === 'turn') {
-                selectors[0].stop().fadeOut(200, function () {
-                    $container.stop().animate({marginLeft: amount}, 300);
-
-                    $left.stop().animate({width: amount}, 300, function () {
-                        selectors[1].stop().fadeIn(200, function () {
-                            callback();
+                if(position === 'left'){
+                    selectors[0].stop().fadeOut(200, function () {
+                        $container.dequeue().animate({marginLeft: amount}, 300);
+                        $left.stop().animate({width: amount}, 300, function () {
+                            selectors[1].stop().fadeIn(200, function () {
+                                callback();
+                            });
                         });
                     });
-                });
+                }else if(position === 'right'){
+                    selectors[0].stop().fadeOut(200, function () {
+                        $container.dequeue().animate({marginRight: amount}, 300);
+                        $right.stop().animate({width: amount}, 300, function () {
+                            selectors[1].stop().fadeIn(200, function () {
+                                callback();
+                            });
+                        });
+                    });
+                }
+
             } else if (mode === 'offset') {
                 selectors[0].css('transform', 'translate( ' + amount + ' )');
                 callback();
             }
         }
 
-        // ---------
-        // 绑定事件
+        /**
+         * ==================
+         *      事件
+         * ==================
+         */
 
         // 事件处理 防止冒泡和默认事件
         function eventHandler(event, selector) {
@@ -152,92 +272,87 @@
         $container.on('click', function (event) {
             if (setting.autoClose && ($left || $right)) {
                 eventHandler(event, $(this));
-                close();
+                close('left');
+                close('right');
             }
         });
 
-        // 打开
-        function open(side) {
-            if (side === 'left' && leftSmActive) {
-                animate('turn', [$left_sm, $left_bg], left_bg_width, function () {
-                    leftBgActive = true;
-                    leftSmActive = false;
-                });
-            } else if (side === 'left' && $left) {
-                if(setting.leftMode === 'container-offset'){
-                    animate(setting.leftMode, [$container], left_width_unit, function () {
-                        leftActive = true;
-                    });
-                }else if(setting.leftMode === 'sidebar-offset'){
-                    animate('offset', [$left], '0px', function () {
-                        leftActive = true;
-                    });
-                }
 
-            }
-
-        }
-
-        // 关闭
-        function close(side) {
-            if (side === 'left' && leftBgActive) {
-                animate('turn', [$left_bg, $left_sm], left_sm_width, function () {
-                    leftBgActive = false;
-                    leftSmActive = true;
-                });
-            } else if (side === 'left' && $left) {
-                if(setting.leftMode === 'container-offset'){
-                    animate('offset', [$container], '0px', function () {
-                        leftActive = false;
-                    });
-                }else if(setting.leftMode === 'sidebar-offset'){
-                    animate('offset', [$left], '-'+left_width_unit, function () {
-                        leftActive = false;
-                    });
-                }
-
-
-            }
-        }
-
-        // 开关
-        function toggle(side) {
-            if (side === 'left' && $left) {
-                if (leftActive && $left_bg && $left_sm) {
-                    if (leftBgActive) {
-                        close('left');
-                    } else if (leftSmActive) {
-                        open('left');
-                    }
-                } else {
-                    if (!leftActive) {
-                        open('left');
-
-                    } else {
-                        close('left');
-                    }
-                }
-            }
-            if (side === 'right' && $right) {
-                if (rightActive && $right_bg && $right_sm) {
-                    if (leftBgActive) {
-                        close('right');
-                    } else if (leftSmActive) {
-                        open('right');
-                    }
-                } else {
-                    if (!rightActive) {
-                        open('right');
-                    } else {
-                        close('right');
-                    }
-                }
-            }
-
-        }
+        /**
+         * ==================
+         *       接口
+         * ==================
+         */
 
         this.toggle = toggle;
         this.close = close;
         this.open = open;
+        this.destroy = function(side){
+            if(side === 'left' && $left){
+                if(leftActive) close('left');
+                setTimeout(function(){
+                    $left.remove();
+                    $left = false;
+                },400);
+            }
+            if(side === 'right' && $right){
+                if(rightActive) close('right');
+                setTimeout(function(){
+                    $right.remove();
+                    $right = false;
+                },400);
+            }
+        };
+
+        // 初始化插件
+        (function init() {
+            if($container){
+                // 左侧边栏
+                if($left){
+                    if(setting.leftMode === 'sidebar-turn' && $left_bg && $left_sm){// 如果切换模式为大小交替隐藏模式
+                        leftActive = true;
+                        if (setting.leftTurnShow === 'jqsb-left-bg') { // 默认显示大的话,隐藏小的  反之,亦然
+                            $container.css('marginLeft', left_bg_width_unit);// 初始 $container 内容容器的左外补 margin-left
+                            $left.width(left_bg_width_unit);
+                            $left_sm.hide();
+                            leftBgActive = true;
+                        } else {
+                            $container.css('marginLeft', left_sm_width_unit);// 初始 $container 内容容器的左外补 margin-left
+                            $left.width(left_sm_width_unit);
+                            $left_bg.hide();
+                            leftSmActive = true;
+                        }
+                    }else if(setting.leftMode === 'container-offset' && leftActive){
+                        open('left');
+                    }else if(setting.leftMode === 'sidebar-offset'){ // 如果切换模式为侧边栏滑动
+                        $left.css('marginLeft','-'+left_width_unit).addClass('sidebar-mode');
+                        if(leftActive) open('left');
+                    }
+                }
+                // 右侧边栏
+                if($right){
+                    if(setting.rightMode === 'sidebar-turn' && $right_bg && $right_sm){// 如果切换模式为大小交替隐藏模式
+                        rightActive = true;
+                        if (setting.rightTurnShow === 'jqsb-right-bg') { // 默认显示大的话,隐藏小的  反之,亦然
+                            $container.css('marginRight', right_bg_width_unit);// 初始 $container 内容容器的左外补 margin-left
+                            $right.width(right_bg_width_unit);
+                            $right_sm.hide();
+                            rightBgActive = true;
+                        } else {
+                            $container.css('marginRight', right_sm_width_unit);// 初始 $container 内容容器的左外补 margin-left
+                            $right.width(right_sm_width_unit);
+                            $right_bg.hide();
+                            rightSmActive = true;
+                        }
+                    }else if(setting.rightMode === 'container-offset' && rightActive){
+                        open('right');
+                    }else if(setting.rightMode === 'sidebar-offset'){ // 如果切换模式为侧边栏滑动
+                        $right.css('marginRight','-'+right_width_unit).addClass('sidebar-mode');
+                        if(rightActive) open('right');
+                    }
+                }
+            }
+        })();
+
     };
 })(jQuery);
